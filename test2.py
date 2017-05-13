30ed116ac17ce71861608275918250b2f201c911
LeonChen66 <leo0215667@gmail.com>
2017-05-13 20:39:34 +0800

    test2.py

--- test2.py
+++ test2.py
@@ -6,13 +6,17 @@ document = docx.Document('研究助理學經歷表.docx')
 
 table = document.tables
 
-# for row in table[0].rows:
-#     for cell in row.cells:
-#         for paragraph in cell.paragraphs:
-#             inline = paragraph.runs
-#             for i in range(len(inline)):
-#                 inline[i].text = "安安"
-#                 inline[i].bold = True
+for row in table[0].rows:
+    for cell in row.cells:
+        for paragraph in cell.paragraphs:
+            if paragraph.text =="陳立恒":
+                print(paragraph.text)
+                # paragraph.text = "哈哈哈"
+            # inline = paragraph.runs
+            # for i in range(len(inline)):
+            #     # inline[i].text = "安安"
+            #     print(inline[i].text)
+            #     inline[i].bold = True
 
 row = table[0].rows[0]
 
