fe255ac72a88d1dde6c2b1dd5675925541915799
LeonChen66 <leo0215667@gmail.com>
2017-05-13 20:45:13 +0800

    test2.py

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 import docx
 
 
-document = docx.Document('研究助理學經歷表.docx')
+document = docx.Document('james.docx')
 
 
 table = document.tables
@@ -9,19 +9,20 @@ table = document.tables
 for row in table[0].rows:
     for cell in row.cells:
         for paragraph in cell.paragraphs:
-            if paragraph.text =="陳立恒":
-                print(paragraph.text)
-                # paragraph.text = "哈哈哈"
-            # inline = paragraph.runs
-            # for i in range(len(inline)):
-            #     # inline[i].text = "安安"
-            #     print(inline[i].text)
-            #     inline[i].bold = True
+            # if paragraph.text =="aa":
+            #     print(paragraph.text)
+            #     paragraph.text = "哈哈哈"
+            inline = paragraph.runs
+            for i in range(len(inline)):
+                if inline[i].text == "陳":
+                    inline[i].text = "asdjfl"
+                # print(inline[i].text)
+                # inline[i].bold = True
 
-row = table[0].rows[0]
-
-cell = row.cells[6]
-
-paragraph = cell.paragraphs[0]
+# row = table[0].rows[0]
+#
+# cell = row.cells[6]
+#
+# paragraph = cell.paragraphs[0]
 print(paragraph.text)
 document.save('test.docx')
