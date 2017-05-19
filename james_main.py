64d2d91b98025c9e18aa5ab8b16fdd7d7ce66e95
LeonChen66 <leo0215667@gmail.com>
2017-05-20 01:58:22 +0800

    james_docx

---
+++ james_main.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtWidgets, QtGui
+import sys
+
+from james import Ui_Form    # 导入生成form.py里生成的类
+
+class mywindow(QtWidgets.QWidget,Ui_Form):
+    def __init__(self):
+        super(mywindow,self).__init__()
+        self.setupUi(self)
+
+    #定义槽函数
+    def hello(self):
+        self.textEdit.setText("hello world")
+
+if __name__=="__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = mywindow()
+    window.show()
+    sys.exit(app.exec_())
--- test2.py
+++ test2.py
@@ -1,23 +1,27 @@
 import docx
+import os
 
-
-document = docx.Document('james.docx')
+docx_name = input('Please input File Name ：')
+docx_name = docx_name + '.docx'
+print(docx_name)
+document = docx.Document('example.docx')
 
 
 table = document.tables
 
-for row in table[0].rows:
-    for cell in row.cells:
-        for paragraph in cell.paragraphs:
-            # if paragraph.text =="aa":
-            #     print(paragraph.text)
-            #     paragraph.text = "哈哈哈"
-            inline = paragraph.runs
-            for i in range(len(inline)):
-                if inline[i].text == "陳":
-                    inline[i].text = "asdjfl"
-                # print(inline[i].text)
-                # inline[i].bold = True
+def auto_word_cell(replaced_word,content):
+    for row in table[0].rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    # if paragraph.text =="aa":
+                    #     print(paragraph.text)
+                    #     paragraph.text = "哈哈哈"
+                    inline = paragraph.runs
+                    for i in range(len(inline)):
+                        if inline[i].text == replaced_word:
+                            inline[i].text = content
+                        # print(inline[i].text)
+                        # inline[i].bold = True
 
 # row = table[0].rows[0]
 
@@ -25,5 +29,8 @@ for row in table[0].rows:
 # cell = row.cells[6]
 #
 # paragraph = cell.paragraphs[0]
-print(paragraph.text)
-document.save('test.docx')
+# print(paragraph.text)
+
+auto_word_cell('aa','hahaha')
+document.save(docx_name)
+os.system('pause')
